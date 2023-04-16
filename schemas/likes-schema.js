0f87811735f5a7891a5208793d132b59aa16c993
K Vivekananda Reddy <vivekananda-reddy@users.noreply.github.com>
2023-04-16 03:19:23 -0400

    working version of likes

---
+++ schemas/likes-schema.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+
+const likesSchema = new mongoose.Schema(
+    {
+        idMeal: {type:String, required:true},
+        strMeal: {type:String, required:true},
+        strMealThumb: {type:String, required:true},
+        user: { type: mongoose.Schema.Types.ObjectId, ref: "users" },
+        role: {type:String, required:true}
+
+    }, {collection: 'likes'});
+export default likesSchema;
