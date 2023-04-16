f2cc653f6fa004659fa12b710148dc9b7199f15e
K Vivekananda Reddy <vivekananda-reddy@users.noreply.github.com>
2023-04-16 12:31:34 -0400

    added createdAt field

--- schemas/likes-schema.js
+++ schemas/likes-schema.js
@@ -6,7 +6,8 @@ const likesSchema = new mongoose.Schema(
         strMeal: {type:String, required:true},
         strMealThumb: {type:String, required:true},
         user: { type: mongoose.Schema.Types.ObjectId, ref: "users" },
-        role: {type:String, required:true}
+        role: {type:String, required:true},
+        createdAt : {type: Date, default: Date.now()}
 
     }, {collection: 'likes'});
 export default likesSchema;
